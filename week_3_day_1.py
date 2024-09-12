9aeef85ce41b15ad5aca84232bbfee57ee4d18ed
Gorski <jdgorski@cps.k12.il.us>
2024-09-12 14:43:49 -0500

    Basic Math Calculator

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -178,20 +178,34 @@
 
 ##########################################################################################
 # #addition
-# #multiplication
-# #division
-# #modulo
-# #powers
-# #get the max and min of a number
-# #round a number
-# # absolute value
-# # order of operations
+# print(2+1)
+# # #multiplication
+# print(2*2)
+# # #division
+# print(3/2)
+# # #modulo
+# For remainder of numbers
+# print(7%4)
+# # #powers
+# print(2**3)
+# # #get the max and min of a number
+# print(max(1,2))
+# print(min(1,2))
+# # #round a number
+# print(round(3.4))
+# # # absolute value
+# print(abs(-3))
+# # # order of operations
+# print(2+10*10+3)
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *    
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print(floor(3.7))
 # #ceil method
+print(ceil(3.7))
 # #sqrt method
+print(sqrt(36))
 
 
 
@@ -207,18 +221,27 @@
 
 
 ##########################################################################################
-# So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
-# # **Getting Input from users**
-# #how do we get input from users?
-# input("what is your name?")
-# # basic math calculator
-# #ask the user for 2 numbers
-# # print out a statement where you:
-# # add them together
-# #multiply
-# # find the max number
-# # find the remainder of the numbers
-# #round one number
+# # So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
+# # # **Getting Input from users**
+# # #how do we get input from users?
+# name = input("What is your name? ")
+# # input("what is your name?")
+# print("hello" + name)
+# # # basic math calculator
+# # #ask the user for 2 numbers
+# num1 = int(input("Pick a number: "))
+# num2 = int(input("Pick another number: "))
+# # # print out a statement where you:
+# print(f'{num1 + num2}')
+# # # add them together
+# # #multiply
+# print(f'{num1*num2}')
+# # # find the max number
+# print(max(num1,num2))
+# # # find the remainder of the numbers
+# print((num1%num2))
+# # #round one number
+# print(round(num1))
 
 
 
@@ -228,6 +251,28 @@
 
 
 ##########################################################################################
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 % num2)
+print(num1 / num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # # mad libs game
 # print("Roses are {color}")
 # print("{plural noun} are blue")
